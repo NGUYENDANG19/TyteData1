80ef73a2633f2c1d6ccd80e69313f9a130da5fb9
NGUYENDANG19 <113655612+NGUYENDANG19@users.noreply.github.com>
2022-10-16 16:41:31 +0700

    Add files via upload

---
+++ Worldspace/src/Nguyen/dev/MainApp.java
@@ -0,0 +1,35 @@
+package Nguyen.dev;
+import java.util.Scanner;
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a=5;
+		int b=14;
+		
+		System.out.println("a==");
+		//a = sc.nextInt();
+		
+		System.out.println("b==");
+		//b = sc.nextInt();
+		
+		if (a==0)	{
+		if (b==0);	{
+			System.out.println("pt vo so nghiem");
+		}	{
+			System.out.println("pt  vo nghiem");
+
+		}
+		
+				
+		
+	}else	{
+		float nghiem = -b/a;
+		System.out.println("pt co nghiem");
+
+	}
+	}	
+}
+
+
---
+++ Worldspace/src/Nguyen2/dev/MainApp.java
@@ -0,0 +1,21 @@
+package Nguyen2.dev;
+import java.util.Scanner;
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a,b ;
+		System.out.println("a==");
+		a = sc.nextInt();
+		
+		System.out.println("b==");
+		b = sc.nextInt();
+		
+		int tong = a+b ;
+		System.out.println(a+"+"+b+"="+tong);
+		
+	}
+
+}
